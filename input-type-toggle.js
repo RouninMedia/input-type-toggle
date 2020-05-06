518f710c6d40255700b948e8074639adcd7494d2
Alan Lansdowne <daimyou@rounin.co.uk>
2020-05-06 14:41:04 +0100

    Update input-type-toggle.js

--- input-type-toggle.js
+++ input-type-toggle.js
@@ -1,1 +1,20 @@
+const toggleCheckbox = document.querySelector('.checkboxToggle input[type="checkbox"]');
 
+const reviewCheckboxStatus = () => {
+
+  const checkboxStatus = document.querySelector('[data-checkbox-status]');
+  
+  if (toggleCheckbox.checked === true) {
+  
+    checkboxStatus.dataset.checkboxStatus = 'on';
+    checkboxStatus.textContent = checkboxStatus.dataset.checkboxStatus;
+  }
+  
+  else {
+  
+    checkboxStatus.dataset.checkboxStatus = 'off';
+    checkboxStatus.textContent = checkboxStatus.dataset.checkboxStatus;
+  }
+}
+
+toggleCheckbox.addEventListener('click', reviewCheckboxStatus, false);
